1f697a304f2cb71a41b60849e8d1b9a321bf560d
Mohammed Yusuf <mohammedyusuf445@gmail.com>
2023-07-13 20:28:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+const currentUser = 'mohammed yusuf';
+//const WelcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const welcomeMessage = "Welcome to Flatbook, " + currentUser +'!'
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+
+const firstInitial = currentUser[0];
+const shortGreeting = "Welcome, " + firstInitial + '!';
+
